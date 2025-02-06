5e46f72f64479a861fa05e114683d4c7ba7d9751
xstar97 <dev.xstar97@gmail.com>
2025-02-06 13:11:02 +0000

    Initial commit

---
+++ scripts/flux/updater.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Source the utility file
+source /workspaces/cluster/scripts/utils.sh
+
+# Check if the following are installed
+check_command "flux"
+
+# Reconcile the Flux Git source
+echo "Reconciling Flux Git source..."
+if flux reconcile source git cluster cluster --verbose; then
+    echo "Reconciled successfully."
+else
+    echo "Failed to reconcile. Please check the Flux logs for more details."
+    exit 1
+fi
